0af1595ef33bbc052524ed59183dc180182ced45
Come Vitis <56791111+ShikoteiCoding@users.noreply.github.com>
2025-07-20 22:19:39 +0800

    http async requester to use later

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 # Python development
 format:
-	uv run --active ruff check
+	uv run --active ruff check --fix
 
 sync:
 	uv sync --active
--- src/main.py
+++ src/main.py
@@ -1,2 +1,40 @@
+from requester import request
+
+from aiohttp import ClientSession
+
+import asyncio
+
+def parse_request_params(params: dict) -> dict:
+    """
+    Parse sql parameters to function parameters
+    """
+    return {
+        "url": params["url"],
+        "method": params["method"],
+        "jsonpath": params["json.jsonpath"]
+    }
+
+
+async def main():
+
+    params = {
+        "connector": "http",
+        'url': 'https://httpbin.org/get',
+        'method': 'GET',
+        'scan.interval': '60s',
+        'json.jsonpath': '$.url'
+    }
+
+    client = ClientSession()
+
+    res = await request(client, **parse_request_params(params))
+
+    await client.close()
+
+    return res
+
 if __name__ == "__main__":
-    print("OMLSP")
+    print("OMLSP starting")
+    loop = asyncio.get_event_loop()
+    res = loop.run_until_complete(main())
+    print(res)
