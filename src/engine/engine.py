6bef723bc6e4a33c93c9352a18dc18775f064a6c
ShikoteiCoding <56791111+ShikoteiCoding@users.noreply.github.com>
2025-10-03 09:58:24 +0800

    Last fixes (#58)
    * Deprecate asyncio + revamp scheduler (#42)
    
    * init task handling
    
    * add kafka sink
    
    * add async requester trio
    
    * update example
    
    * minimal scheduler
    
    * split build/run + add build sink connector
    
    * change schedule
    
    * requester
    
    * custom scheduler with trio
    removing asyncio in http requests
    moving channel to root (not the best PR to do but it was pending)
    remove now useless dependencies: aiohttp + trio-asyncio
    
    * change back properites
    
    * rename
    
    * rearrange code
    
    * fix source task scheduler
    
    * change from sink to engine, and rename functions
    
    * remove trio-asyncio
    
    * remove none df
    
    * deprecate asyncio (#43)
    
    * deprecate asyncio in client
    
    * remove asyncio from pytest
    
    * move pytest trio to dev group
    
    * fix typo
    
    * add dataclass (#45)
    
    * add dataclass
    
    * change optional
    
    * change joins type
    
    ---------
    
    Co-authored-by: E229866E <adrien.libert@etu.univ-nantes.fr>
    Co-authored-by: Adrien Libert <82459159+AdrienLibert@users.noreply.github.com>
    
    * update uv
    
    * last fixes
    
    ---------
    
    Co-authored-by: E229866E <adrien.libert@etu.univ-nantes.fr>
    Co-authored-by: Adrien Libert <82459159+AdrienLibert@users.noreply.github.com>

--- src/engine/engine.py
+++ src/engine/engine.py
@@ -459,7 +459,7 @@ async def transform_executable(
     pl_ctx: pl.SQLContext,
 ) -> pl.DataFrame:
     pl_ctx.register(first_upstream, df)
-    transform_df = pl_ctx.execute(transform_query, eager=True)
+    transform_df = pl_ctx.execute(transform_query)
 
     batch_id = get_batch_id_from_view_metadata(conn, name, is_materialized)
     epoch = int(time.time() * 1_000)
