9c20a2fa18396c6b7db792784efb032e32e7210d
E229866E <adrien.libert@etu.univ-nantes.fr>
2025-10-08 21:36:48 +0800

    outside context

--- src/main.py
+++ src/main.py
@@ -42,11 +42,13 @@ async def main():
     app.add_dependency(task_manager)
     app.connect_task_manager(task_manager)
 
+    # Connect Scheduler to TaskManager
+    task_manager.add_dependency(scheduler)
+    task_manager.connect_scheduler(scheduler)
+
     async with trio.open_nursery() as nursery:
         token = trio.lowlevel.current_trio_token()
         scheduler._configure({"_nursery": nursery, "_trio_token": token})
-        task_manager.add_dependency(scheduler)
-        task_manager.connect_scheduler(scheduler)
 
         nursery.start_soon(app.start, nursery)
         for sql in iter_sql_statements(sql_filepath):
