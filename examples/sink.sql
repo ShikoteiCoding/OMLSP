146e7aa549222016265caa01f075fef8002b6392
Come Vitis <56791111+ShikoteiCoding@users.noreply.github.com>
2025-11-01 22:56:25 +0800

    new types for properties

---
+++ examples/sink.sql
@@ -0,0 +1,22 @@
+-- Get all tickers from kucoin
+CREATE TABLE all_tickers (
+    symbol STRING,
+    symbolName STRING,
+    buy FLOAT,
+    sell FLOAT
+)
+WITH (
+    connector = 'http',
+    url = 'https://api.kucoin.com/api/v1/market/allTickers',
+    method = 'GET',
+    schedule = '*/1 * * * *',
+    jq = '.data.ticker[:2][] | {symbol, symbolName, buy, sell}',
+    'headers.Content-Type' = 'application/json'
+);
+
+CREATE SINK all_tickers_sink_bis FROM all_tickers
+WITH (
+    connector = 'kafka',
+    topic = 'tickers_topic_2',
+    server = 'localhost:9092',
+);
